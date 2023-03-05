4971608c89ff0812f735463edb131aa3b1528eff
pieecia <piotr.kananowicz@elpassion.pl>
2023-03-05 22:28:04 +0100

    feat: add next button variant

--- test-3/src/components/button/index.tsx
+++ test-3/src/components/button/index.tsx
@@ -1,16 +1,19 @@
 import classNames from 'classnames';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: 'primary' | 'secondary';
+  variant: 'primary' | 'secondary';
 }
 
 export const Button = ({ variant = 'primary', ...props }: ButtonProps) => {
   return (
     <button
-      className={classNames('rounded-full px-6 py-2 text-white font-bold', {
-        'bg-blue-500 hover:bg-blue-600': variant === 'primary',
-        'bg-gray-500 hover:bg-gray-600': variant === 'secondary',
-      })}
+      className={classNames(
+        'rounded-full px-10 py-2.5 text-white font-bold uppercase text-sm transition duration-200 ease-in-out tracking-wider',
+        {
+          'bg-blue-700 hover:bg-blue-800': variant === 'primary',
+          'bg-gray-700 hover:bg-gray-800': variant === 'secondary',
+        }
+      )}
       {...props}
     >
       {props.value}
