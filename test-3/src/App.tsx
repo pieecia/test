16c817bf8dc2d5c0f683570314615a08fc84313a
pieecia <piotr.kananowicz@elpassion.pl>
2023-03-07 00:23:55 +0100

    fix: bg

--- test-3/src/App.tsx
+++ test-3/src/App.tsx
@@ -4,7 +4,7 @@ import { FiltersProvider } from './contexts/filters';
 
 function App() {
   return (
-    <div className="bg-gray-100 h-screen">
+    <div className="bg-gray-100 min-h-screen">
       <div className="container mx-auto max-w-5xl">
         <FiltersProvider>
           <Filters />
