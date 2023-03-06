7762078ef8f3358fb4ba2e70c83c1fca4c916184
pieecia <piotr.kananowicz@elpassion.pl>
2023-03-06 22:10:13 +0100

    feat: update dropdown data

--- test-3/src/components/search/index.tsx
+++ test-3/src/components/search/index.tsx
@@ -1,10 +1,30 @@
 import { Dropdown } from '../dropdown';
 
-const options = [
+const sortOptions = [
   { name: 'Wszystkie', id: 'all' },
-  { name: '1', id: '1' },
-  { name: '2', id: '2' },
-  { name: '3', id: '3' },
+  { name: 'Cena', id: 'price' },
+  { name: 'Pojemność', id: 'capacity' },
+];
+
+const featuresOptions = [
+  { name: 'Wszystkie', id: 'all' },
+  { name: 'Panel AI Control', id: 'panelAIControl' },
+  { name: 'Silnik inwerterowy', id: 'silnikInwerterowy' },
+  { name: 'Wyświetlacz elektroniczny', id: 'wyświetlaczElektroniczny' },
+];
+
+const energyClassOptions = [
+  { name: 'Wszystkie', id: 'all' },
+  { name: 'A', id: 'A' },
+  { name: 'B', id: 'B' },
+  { name: 'C', id: 'C' },
+];
+
+const capacityOptions = [
+  { name: 'Wszystkie', id: 'all' },
+  { name: '8kg', id: '8kg' },
+  { name: '9kg', id: '9kg' },
+  { name: '10.5kg', id: '10.5kg' },
 ];
 
 export const Search = () => {
@@ -12,21 +32,21 @@ export const Search = () => {
     <div className="grid grid-cols-4 gap-x-3 mb-4">
       <div>
         <label className="block text-sm font-bold text-black text-lg mb-2">Sortuj po</label>
-        <Dropdown options={options} />
+        <Dropdown options={sortOptions} />
       </div>
       <div>
         <label className="block text-sm font-bold text-black text-lg mb-2">Funkcje</label>
-        <Dropdown options={options} />
+        <Dropdown options={featuresOptions} />
       </div>
       <div>
         <label className="block text-sm font-bold text-black text-lg mb-2">
           Klasa energetyczna
         </label>
-        <Dropdown options={options} />
+        <Dropdown options={energyClassOptions} />
       </div>
       <div>
         <label className="block text-sm font-bold text-black text-lg mb-2">Pojemność</label>
-        <Dropdown options={options} />
+        <Dropdown options={capacityOptions} />
       </div>
     </div>
   );
